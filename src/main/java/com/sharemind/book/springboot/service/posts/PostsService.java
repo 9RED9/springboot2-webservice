d76f34c7a150e43a0115a945e435e579133fdf3c
0myrest0 <0myrest0@gmail.com>
2020-07-08 17:46:11 +0900

    page123 study

--- src/main/java/com/sharemind/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sharemind/book/springboot/service/posts/PostsService.java
@@ -1,9 +1,13 @@
 package com.sharemind.book.springboot.service.posts;
 
+import com.sharemind.book.springboot.domain.posts.Posts;
 import com.sharemind.book.springboot.domain.posts.PostsRepository;
+import com.sharemind.book.springboot.web.dto.PostsResponseDto;
 import com.sharemind.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sharemind.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +15,25 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/sharemind/book/springboot/web/PostApiController.java
+++ src/main/java/com/sharemind/book/springboot/web/PostApiController.java
@@ -1,11 +1,12 @@
 package com.sharemind.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+
+import com.sharemind.book.springboot.web.dto.PostsResponseDto;
+import com.sharemind.book.springboot.web.dto.PostsUpdateRequestDto;
 import com.sharemind.book.springboot.service.posts.PostsService;
 import com.sharemind.book.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +17,17 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requesDto) {
+
+        return postsService.update(id, requesDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+
+        return postsService.findById(id);
     }
 }
